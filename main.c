a06b74fe9f09c3b58db0afa76e956b79333f9b35
LaGabaRDK64 <gabrielagosto10.2007@gmail.com>
2026-05-10 12:39:50 -0500

    Datos distribuidos en main.c

--- main.c
+++ main.c
@@ -5,7 +5,14 @@
 
 int main(){
 
-    int opcmain;
+    int opcmain; /* >>>> Opcion seleccionada para el switch*/
+
+    char nameproductmain[15][25]; /*Puede almacenar hasta 15 nombres, con 25 caracteres*/
+    float tiempodeproduccion[15]  = {0}; /* >>>> Puedes agregar el tiempo de produccion de cada producto (max >>> 15)*/
+    float materiaprima[15] = {0}; /* >>>> Informacion materia prima necesaria para cada producto (de cada producto individual)*/
+    float energiatotal[15] = {0}; /* >>>> Informacion de la energia total de cada producto (de manera individual)*/
+    int numerosdeproductos = 0; /* >>> Cantidad de prodcutos almacenadas en el sistema, numero maximo de prodcutos 15*/
+    float trabajo[15] = {0}; /* >>>> Numero de trabajo requerido para cada producto (de manera individual)*/
 
     welcomeMsg();
 
@@ -37,6 +44,12 @@ int main(){
             /*Eliminar producto*/
         
             break;
+        
+        case 4:
+
+            pause();
+
+            /*Calcular eficiencia (dani aca pon tu parte)*/
         default:
 
         break;
