86d31cb9217be31f09240f1fce655bb047811ac0
LaGabaRDK64 <gabrielagosto10.2007@gmail.com>
2026-05-09 23:29:59 -0500

    betterio.c y betterio.h llenos, funciones listas para usar :D

---
+++ betterio.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "betterio.h"
+
+/*Validacion de caracteres*/
+
+void stringverification(char caracter[]){
+    
+    char caracterview[25];
+    
+    fgets(caracterview,25,stdin);
+    
+    /*Si el primer valor de word es igual a un espacio vacio o a salto de linea te devuelva */
+    if (caracterview[0] == ' ' || caracterview[0] == '\n')
+    {
+        printf("||| ERROR  ||| :  El programa no admite espacios en blanco \n");
+        printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+        stringverification(caracter);
+        return;
+    }
+    
+    /*Elimina el espacio*/
+    eliminatespace(caracterview);
+    strcpy(caracter,caracterview);
+
+}
+
+/*Elimina espacios*/
+
+void eliminatespace(char word[]){
+
+    int longitud;
+    int rango = 25;    
+
+    for (int i = 0; i < rango; i++)
+    {
+        if (word[i] == '\n')
+        {
+            word[i] = '\0';
+        }
+        
+    }
+
+}
+
+/*Validacion de entrada numerica (Con numeros enteros)entre un minimo y un maximo*/
+
+int ENTnumberverification(int max, int min){
+
+    int num;
+    char buffer[25];
+    char *ptr;
+
+    do
+    {
+        fgets(buffer,5,stdin);
+        /*El buffer guarda el string*/
+        num = strtol(buffer, &ptr , 10);
+
+        if(buffer[0] == ' ' || buffer[0] == '\n')
+        {
+
+            printf("||| ERROR ||| : El programa no admite espacios en blanco \n");
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }else if (ptr == buffer)
+        {
+
+            printf("||| ERROR ||| : El programa no admite letras ");
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }else if (*ptr != '\n' && *ptr != '\0')
+        {
+            
+            printf("||| ERROR ||| : El programa no admite ese tipo de valor");
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }
+        else if(num < min)
+        {
+
+            printf("||| ERROR ||| : El programa no admite numeros menores a %d ", min);
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }else if (num > max)
+        {
+
+            printf("||| ERROR ||| : El programa no admite numeros mayores a %d ", max);
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }
+
+    }while (num < min || num > max || ptr == buffer);
+
+    return num;
+
+}
+
+/*Validacion de entrada numerica (Con numeros enteros)entre un minimo y un maximo*/
+
+float DECnumberverification(int max, int min){
+
+    float num;
+    char buffer[25];
+    char *ptr;
+
+    do
+    {
+        fgets(buffer,5,stdin);
+        /*El buffer guarda el string*/
+        num = strtof(buffer, &ptr);
+
+        if(buffer[0] == ' ' || buffer[0] == '\n')
+        {
+
+            printf("||| ERROR ||| : El programa no admite espacios en blanco \n");
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }else if (ptr == buffer)
+        {
+
+            printf("||| ERROR ||| : El programa no admite letras ");
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }else if (*ptr != '\n' && *ptr != '\0')
+        {
+            
+            printf("||| ERROR ||| : El programa no admite ese tipo de valor");
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }
+        else if(num < min)
+        {
+
+            printf("||| ERROR ||| : El programa no admite numeros menores a %d ", min);
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }else if (num > max)
+        {
+
+            printf("||| ERROR ||| : El programa no admite numeros mayores a %d ", max);
+            printf("Porfavor vuelva a ingresar su opcion aqui >>>> ");
+
+        }
+
+    }while (num < min || num > max || ptr == buffer);
+
+    return num;
+
+}
--- betterio.h
+++ betterio.h
@@ -1,6 +1,13 @@
 #ifndef betterio_h
 #define betterio_h
 
-
+/*Verifica cadena de caracteres*/
+void stringverification(char caracter[]);
+/*Elimina espacios de los textos*/
+void eliminatespace(char word[]);
+/*El numero entero ingresado entra a procesos de verificacion entre maximos y minimos, que no sean datos basuras o letras*/
+int ENTnumberverification(int max, int min);
+/*El numero decimal ingresado entra a procesos de verificacion entre maximos y minimos, que no sean datos basuras o letras*/
+float DECnumberverification(int max, int min);
 
 #endif
